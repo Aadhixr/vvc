b529ae54f8cbb1f2fae3353e9a4bc55f61138960
Aadhixr <129429716+Aadhixr@users.noreply.github.com>
2024-03-27 01:22:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,6 +39,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Aaxx_fikes')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001536186448'))
+PORT = environ.get("PORT", "8080")
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'AboutAadhi')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
